82c0a64da6bf20459bb02be12fc7c9ff76f81c4c
FindCars24 <60684925+FindCars24@users.noreply.github.com>
2020-02-05 07:10:21 +0100

    Create preisbewertung.php

---
+++ preisbewertung.php
@@ -0,0 +1,8 @@
+<?php
+// Die Fahrzeug Id
+// Zu ersetzen durch korrekte Id
+$adId = 123456;
+
+// iframe ausgeben
+echo '<iframe height="200" width="340" frameborder="0" src="https://www.findcars24.de/api/rating?adId=' . $adId . '"></iframe>';
+?>
